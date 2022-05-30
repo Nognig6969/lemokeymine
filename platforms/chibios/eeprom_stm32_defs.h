1a3ea6c1dfb30d05e494c672e5ad56fae6d353ae
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2022-05-30 15:25:39 +0800

    Update lib

--- platforms/chibios/eeprom_stm32_defs.h
+++ platforms/chibios/eeprom_stm32_defs.h
@@ -25,7 +25,7 @@
 #        ifndef FEE_PAGE_COUNT
 #            define FEE_PAGE_COUNT 2 // How many pages are used
 #        endif
-#    elif defined(STM32F103xE) || defined(STM32F303xC) || defined(STM32F072xB) || defined(STM32F070xB) || defined(STM32L432xB)
+#    elif defined(STM32F103xE) || defined(STM32F303xC) || defined(STM32F303xE) || defined(STM32F072xB) || defined(STM32F070xB)
 #        ifndef FEE_PAGE_SIZE
 #            define FEE_PAGE_SIZE 0x800 // Page size = 2KByte
 #        endif
@@ -47,11 +47,11 @@
 #        define FEE_MCU_FLASH_SIZE 32 // Size in Kb
 #    elif defined(GD32VF103C8)
 #        define FEE_MCU_FLASH_SIZE 64 // Size in Kb
-#    elif defined(STM32F103xB) || defined(STM32F072xB) || defined(STM32F070xB) || defined(STM32L432xB) || defined(GD32VF103CB)
+#    elif defined(STM32F103xB) || defined(STM32F072xB) || defined(STM32F070xB) || defined(GD32VF103CB)
 #        define FEE_MCU_FLASH_SIZE 128 // Size in Kb
 #    elif defined(STM32F303xC) || defined(STM32F401xC)
 #        define FEE_MCU_FLASH_SIZE 256 // Size in Kb
-#    elif defined(STM32F103xE) || defined(STM32F401xE) || defined(STM32F411xE)
+#    elif defined(STM32F103xE) || defined(STM32F303xE) || defined(STM32F401xE) || defined(STM32F411xE)
 #        define FEE_MCU_FLASH_SIZE 512 // Size in Kb
 #    elif defined(STM32F405xG)
 #        define FEE_MCU_FLASH_SIZE 1024 // Size in Kb
