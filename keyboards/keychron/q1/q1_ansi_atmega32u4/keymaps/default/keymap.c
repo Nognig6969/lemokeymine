2a312fd6a25d2ddb17da55a42f2caeda97e0980b
kenichirouyoshioka <kenichirou@mub.biglobe.ne.jp>
2022-07-02 19:05:55 +0900

    LOPT>LALT  LCOMD>LGUI

--- keyboards/keychron/q1/q1_ansi_atmega32u4/keymaps/default/keymap.c
+++ keyboards/keychron/q1/q1_ansi_atmega32u4/keymaps/default/keymap.c
@@ -145,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,             KC_TILD,
         KC_LCTL,    KC_A,    KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,              KC_GRV,
         KC_LSFT,             KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT, KC_UP,
-        MO(WIN_FN),KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, KC_LEAD, MO(WIN_FN),KC_LEFT, KC_DOWN,    KC_RGHT),
+        MO(WIN_FN),KC_LALT,  KC_LGUI,                               KC_SPC,                                  KC_RGUI,  KC_LEAD, MO(WIN_FN),KC_LEFT, KC_DOWN,    KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_82(
         TO(WIN_BASE),         KC_TRNS,  KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
