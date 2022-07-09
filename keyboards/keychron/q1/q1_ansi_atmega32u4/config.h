89b59c82fad745ff41db8296c3f8925729448517
kenichirouyoshioka <kenichirou@mub.biglobe.ne.jp>
2022-07-09 11:58:02 +0900

    change

--- keyboards/keychron/q1/q1_ansi_atmega32u4/config.h
+++ keyboards/keychron/q1/q1_ansi_atmega32u4/config.h
@@ -17,8 +17,13 @@
 #pragma once
 
 /* USB Device descriptor parameter */
+<<<<<<< Updated upstream
 #define PRODUCT_ID 0x0100
 #define DEVICE_VER 0x0109
+=======
+#define PRODUCT_ID 0x0280
+#define DEVICE_VER 0x0108
+>>>>>>> Stashed changes
 
 /* key matrix size */
 #define MATRIX_ROWS 6
