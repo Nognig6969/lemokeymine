f526bc7ed295efa48b1c3156e6c62c83c94bc153
kenichirouyoshioka <kenichirou@mub.biglobe.ne.jp>
2022-07-20 15:24:48 +0900

    small change 7/20

--- keyboards/keychron/q1/q1_ansi_atmega32u4/keymaps/default/keymap.c
+++ keyboards/keychron/q1/q1_ansi_atmega32u4/keymaps/default/keymap.c
@@ -321,12 +321,12 @@ static uint8_t mac_keycode[4] = { KC_LOPT, KC_ROPT, KC_LCMD, KC_RCMD };
 */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_1] = LAYOUT_ansi_82(
-        TG(LAYER_2),          KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,  KC_PLUS, KC_QUES,    KC_NO,
+        TG(LAYER_2),         KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,  KC_PLUS, KC_QUES,    KC_NO,
         KC_ESC,     TD(TD_1),TD(TD_2), TD(TD_3), TD(TD_4), TD(TD_5), TD(TD_6), TD(TD_7), TD(TD_8), TD(TD_9), TD(TD_10),TD(TD_11),TD(TD_12),KC_BSPC,             KC_WH_U,
         KC_TAB,     KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,             KC_WH_D,
         KC_LCTL,    KC_A,    KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,              KC_GRV,
         KC_LSFT,             KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT, KC_UP,
-        MO(LAYER_4),KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RGUI,  KC_LEAD, MO(LAYER_4),KC_LEFT, KC_DOWN,    KC_RGHT),
+        MO(LAYER_4),KC_LALT, KC_LGUI,                                KC_SPC,                                 KC_RGUI,  KC_RCTL,MO(LAYER_4),KC_LEFT, KC_DOWN,    KC_RGHT),
 
     [LAYER_2] = LAYOUT_ansi_82(
         TO(LAYER_3),       KC_F12,     KC_F2,     KC_F3,      KC_F4,       KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_TRNS, KC_TRNS,
